8bf2f5b29e9aeae2fb668bacb81eecbcdb6ffb0a
Pfck <87673171+Pfck@users.noreply.github.com>
2021-11-05 16:13:24 +0100

    tiny comment changes

--- PROJECT-1/controllers/donneesControllers.js
+++ PROJECT-1/controllers/donneesControllers.js
@@ -1,4 +1,4 @@
- var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
 const saltRounds = 10;
 var session = require('express-session');
 
@@ -6,18 +6,15 @@ function data(req, res)  {
     //ma const est en dure et remplace les donnees de la bdd -----------------------------
     const holis =
     {
-        "name":"Paula", 
-        "email":"paulagaubin@gmail.com",
+        "name":"HELLO WELCOME TO YOUR SESSION", 
+        "email":"paulafck@gmail.com",
         "mdp":"$2b$10$wBxhuAvMb9KiV8RuaVgMSezSvRsTRyP7DHyAUinfz5RTiB2k0Ve8K"
-    };
-    
+    };    
+    //mdp crypté -------------------------------------------------------------------------
     //bcrypt-----etape1hash---------------------------------------------------------------
     //console.log(bcrypt.hashSync(holis.mdp , saltRounds));
-
     //bcrypt-----etape2compare------------------------------------------------------------
-    
     var comparaison = bcrypt.compareSync(req.body.mdp, holis.mdp);
-
 
     //cette if faisait la comparation entre notre data en dure et notre data de input
     if (req.body.email == holis.email && comparaison){
@@ -25,12 +22,14 @@ function data(req, res)  {
         session.name = holis.name;
         console.log(session);
         res.redirect("/users/re");
-
     }else{
-        res.status(403).send("NOPE");
+        res.status(403).send("Forbidden");
     }
+
+    
     console.log('holis');
     //res.post('/donnees');
+
 }
 
 
